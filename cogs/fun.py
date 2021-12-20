9fb2f832996c4c80cb233cbd88a9f56ca9030d89
max49 <massimomarino49@gmail.com>
2021-12-19 20:05:30 -0500

    rip

--- cogs/fun.py
+++ cogs/fun.py
@@ -111,13 +111,13 @@ class Fun(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(name="join", help="join a voice channel")
+    @commands.command(name="joinvc", help="join a voice channel")
     async def joinvc(ctx, *, channel: discord.VoiceChannel = None):
         if channel is None and ctx.author.voice.channel is None:
             return await ctx.send("You are not in a voice channel.")
         await channel.connect()
     
-    @commands.command(name="leave", help="leave a voice channel")
+    @commands.command(name="leavevc", help="leave a voice channel")
     async def leavevc(ctx):
         await ctx.voice_client.disconnect()
 
