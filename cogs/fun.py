27ba373d7cd49ab9ff6b91dd2d4757d9f21c2521
max49 <massimomarino49@gmail.com>
2021-12-19 20:27:22 -0500

    FUN!!!!!

--- cogs/fun.py
+++ cogs/fun.py
@@ -7,6 +7,7 @@ import discord
 import asyncio
 import random
 import requests
+import youtube_dl
 from random import randint
 from datetime import datetime
 from discord.ext import commands
@@ -106,6 +107,43 @@ class Regents(discord.ui.View):
             await interaction.response.send_message('This button isn\'t for you!', ephemeral=True)
             return False
 
+youtube_dl.utils.bug_reports_message = lambda: ''
+
+ytdl_format_options = {
+    'format': 'bestaudio/best',
+    'restrictfilenames': True,
+    'noplaylist': True,
+    'nocheckcertificate': True,
+    'ignoreerrors': False,
+    'logtostderr': False,
+    'quiet': True,
+    'no_warnings': True,
+    'default_search': 'auto',
+    'source_address': '0.0.0.0' # bind to ipv4 since ipv6 addresses cause issues sometimes
+}
+
+ffmpeg_options = {
+    'options': '-vn'
+}
+
+ytdl = youtube_dl.YoutubeDL(ytdl_format_options)
+
+class YTDLSource(discord.PCMVolumeTransformer):
+    def __init__(self, source, *, data, volume=0.5):
+        super().__init__(source, volume)
+        self.data = data
+        self.title = data.get('title')
+        self.url = ""
+
+    @classmethod
+    async def from_url(cls, url, *, loop=None, stream=False):
+        loop = loop or asyncio.get_event_loop()
+        data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url, download=not stream))
+        if 'entries' in data:
+            # take first item from a playlist
+            data = data['entries'][0]
+        filename = data['title'] if stream else ytdl.prepare_filename(data)
+        return filename
 
 class Fun(commands.Cog):
     def __init__(self, bot):
@@ -120,6 +158,39 @@ class Fun(commands.Cog):
     @commands.command(name="leavevc", help="leave a voice channel")
     async def leavevc(self, ctx):
         await ctx.voice_client.disconnect()
+
+    @commands.command(name='play', help='To play song')
+    async def play(self, ctx, url: str):
+        if ctx.voice_client is None:
+            await ctx.invoke(self.joinvc)
+        async with ctx.typing():
+            filename = await YTDLSource.from_url(url, loop=self.bot.loop)
+            ctx.voice_client.play(discord.FFmpegPCMAudio(executable="ffmpeg.exe", source=filename))
+        await ctx.send('**Now playing song**')
+
+    @commands.command(name='pause', help='This command pauses the song')
+    async def pause(self, ctx):
+        if ctx.voice_client.is_playing():
+            await ctx.voice_client.pause()
+            await ctx.send("paused")
+        else:
+            await ctx.send("no.")
+        
+    @commands.command(name='resume', help='Resumes the song')
+    async def resume(self, ctx):
+        if ctx.voice_client.is_paused():
+            await ctx.voice_client.resume()
+            await ctx.send('resumed')
+        else:
+            await ctx.send("no.")
+
+    @commands.command(name='stop', help='Stops the song')
+    async def stop(self, ctx):
+        if ctx.voice_client.is_playing():
+            await ctx.voice_client.stop()
+            await ctx.send('stopped')
+        else:
+            await ctx.send("no.")
 
     @commands.command(name='chat', help="gives you the ability to chat with the bot", pass_context=True)
     async def chat(self, ctx, channel: typing.Optional[discord.TextChannel], *, message: str):
