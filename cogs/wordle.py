1a316814bea0d9e3787d44bf226db2b4ed1b731d
max49 <massimomarino49@gmail.com>
2022-02-24 13:24:45 -0500

    nikl

--- cogs/wordle.py
+++ cogs/wordle.py
@@ -201,6 +201,7 @@ class Wordle(commands.Cog):
         while guess_number < 5:
             if(not game.value):
                 embed=discord.Embed(title="You win!", color=0x00FF00)
+                await guess.add_reaction('✅')
                 if(doingDaily):
                     for i in range(len(profile_data)):
                         if(profile_data[i]['ID'] == ctx.author.id):
