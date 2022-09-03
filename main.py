ecac7b1570e1d2a63774096515241909c1570743
Max49 <massimomarino49@gmail.com>
2022-09-03 16:56:28 -0400

    yes

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ TOKEN = info[0]
 if __name__ == '__main__':
     for filename in os.listdir('cogs'):
         if filename.endswith('.py'):
-            bot.load_extension(f'cogs.{filename[:-3]}')
+            await bot.load_extension(f'cogs.{filename[:-3]}')
 
 
 @bot.event
